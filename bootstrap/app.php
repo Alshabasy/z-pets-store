d9f24524dd67f83cae8f8f4f7d19e14860753068
Alshabasy <amralshabasy10@gmail.com>
2026-06-06 01:41:37 +0300

    fix middleware session set

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,8 +15,13 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->redirectUsersTo('/dashboard');
 
         $middleware->web(append: [
-            \App\Http\Middleware\SecurityHeaders::class,
-        ]);
+    \App\Http\Middleware\SecurityHeaders::class,
+
+    \Illuminate\Cookie\Middleware\EncryptCookies::class,
+    \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+    \Illuminate\Session\Middleware\StartSession::class,
+    \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+]);
 
         $middleware->alias([
             'isAdmin' => \App\Http\Middleware\IsAdmin::class,
